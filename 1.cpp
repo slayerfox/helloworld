3d67749799c1cb653f1e256c8886ec0cdadece3c
slayerfox <easonday@gmail.com>
2018-11-10 17:02:43 +0800

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -7,53 +7,63 @@
 
 int main(int argc,char **argv)
 {
-    char **pptr;
-    char str[INET_ADDRSTRLEN];
-    struct hostent *hptr;
-
-    char host[] = "www.qq.com";
-
-    // 开始获取
-    if((hptr=gethostbyname(host)) == NULL)
+    // gethostbyname
     {
-      printf("gethostbyname error for host: %s: %s\n", host, hstrerror(h_errno));  // 出错信息，并记录在 h_error 中
-      return;
+        char **pptr;
+        char str[INET_ADDRSTRLEN];
+        struct hostent *hptr;
+
+        char host[] = "www.qq.com";
+
+        // 开始获取
+        hptr = gethostbyname(host);
+        if(hptr == NULL)
+        {
+          printf("gethostbyname error for host: %s: %s\n", host, hstrerror(h_errno));  // 出错信息，并记录在 h_error 中
+          return;
+        }
+
+        // 正式主机名
+        printf("official hostname: %s\n", hptr->h_name);
+
+        // 遍历所有的主机别名
+        int alias_count = 0;
+        for(pptr=hptr->h_aliases; *pptr!=NULL; pptr++)
+        {
+            alias_count++;
+            printf("\talias: %s\n", *pptr);
+        }
+        printf("alias_count:%d\n", alias_count);
+
+        // 遍历所有的主机地址
+        switch(hptr->h_addrtype) // 判断 socket 类型
+        {
+          case AF_INET:  // IP 类 为AF_INET
+          {
+              int addr_count = 0;
+            for(pptr = hptr->h_addr_list; *pptr!=NULL; pptr++)
+            {
+                addr_count++;
+                printf("\taddress: %s\n",
+                    inet_ntop(hptr->h_addrtype, *pptr, str, sizeof(str))); // inet_ntop 转换为点分十进制
+
+               // printf("\taddress: %s\n",
+                    // inet_ntoa(*(struct in_addr*)pptr); // inet_ntoa 转换为点分十进制
+            }
+              printf("addr_count:%d\n", addr_count);
+          }
+            break;
+          default:
+            printf("unknown address type\n");
+            break;
+        }
     }
     
-    // 正式主机名
-    printf("official hostname: %s\n", hptr->h_name);
-
-    // 遍历所有的主机别名
-    int alias_count = 0;
-    for(pptr=hptr->h_aliases; *pptr!=NULL; pptr++)
+    // gethostbyaddr
     {
-        alias_count++;
-        printf("\talias: %s\n", *pptr);
+        
     }
-    printf("alias_count:%d\n", alias_count);
-   
-    // 遍历所有的主机地址
-    switch(hptr->h_addrtype) // 判断 socket 类型
-    {
-      case AF_INET:  // IP 类 为AF_INET
-      {
-          int addr_count = 0;
-        for(pptr = hptr->h_addr_list; *pptr!=NULL; pptr++)
-        {
-            addr_count++;
-            printf("\taddress: %s\n",
-                inet_ntop(hptr->h_addrtype, *pptr, str, sizeof(str))); // inet_ntop 转换为点分十进制
-
-           // printf("\taddress: %s\n",
-                // inet_ntoa(*(struct in_addr*)pptr); // inet_ntoa 转换为点分十进制
-        }
-          printf("addr_count:%d\n", addr_count);
-      }
-        break;
-      default:
-        printf("unknown address type\n");
-        break;
-    }
+    
  
     return 0;
 }
